59f673ec9a6ecb095b7d75098b1e340ef4a81ce0
Andrew Tatang <andrewtang0206@gmail.com>
2021-05-10 16:03:34 +0800

    Added solution for Largest Prime Factor

---
+++ largest_prime_factor.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <math.h>
+
+int main (void)
+{
+    long long n = 600851475143;
+    int factor, i;
+    int largestPrimeFactor = 2;
+
+    while (n%2 == 0)
+    {
+        n = n/2;
+    }  
+
+    for (i = 3; i<sqrt(n); i+=2)
+    {
+        while (n%i == 0)
+        {
+            largestPrimeFactor = i;
+            n = n/i;
+        }
+    }
+
+    if (n > 2)
+    {
+        largestPrimeFactor = n;
+    }
+
+    printf("%d", largestPrimeFactor);
+
+    return 0;
+}
