47e4e5fb9ab0d4d2302df899b449b87a837c9f52
Andrew Tatang <andrewtang0206@gmail.com>
2021-05-10 09:32:43 +0800

    Added solution for sum of even fibonacci

---
+++ sum_of_even_fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main ()
+{    
+    int i, sum=0, t1 = 0, t2 = 1;
+    int nextTerm = t1 + t2;
+
+    for (i=0; nextTerm<4000000; i++)
+    {
+        printf("%d, ", nextTerm);
+        t1 = t2;
+        t2 = nextTerm;
+        nextTerm = t1 + t2; 
+
+        if (nextTerm%2 == 0)
+        {
+            sum += nextTerm;
+        }
+    }
+
+    printf("\nThe sum is %d", sum);
+
+    return 0;
+}
